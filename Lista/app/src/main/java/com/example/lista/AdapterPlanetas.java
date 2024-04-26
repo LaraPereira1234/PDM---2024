ed6ddc7dc4d8ba696e2862aef9a619e905038bf4
Lara <lara.p2006@aluno.ifsc.edu.br>
2024-04-26 11:45:47 -0300

    Lista Completa

--- Lista/app/src/main/java/com/example/lista/AdapterPlanetas.java
+++ Lista/app/src/main/java/com/example/lista/AdapterPlanetas.java
@@ -1,19 +1,34 @@
 package com.example.lista;
 
+import static java.security.AccessController.getContext;
+
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class AdapterPlanetas extends ArrayAdapter<> {
+public class AdapterPlanetas extends ArrayAdapter<Planet> {
 
+    Object[] mObjects;
 
-    public AdapterPlanetas(Context context, int resource, Object[] objects){
+    public AdapterPlanetas(Context context, int resource, Planet[] objects) {
         super(context, resource, objects);
+        mObjects = objects;
     }
 
-    public View getView(int position, View convertView, ViewGroup parent){
-        View itemlista = LayoutInflater.from(getContext()).inflate(R.layout.item_lista_planeta,)
-        TextView tv = itemlista.findViewById(R.id.textViewNomePlaneta);
-        Planet getItem(position);
-                return
+
+    public View getView(int position, View conterView, ViewGroup parent){
+        View itemlista = LayoutInflater.from(getContext()).inflate(R.layout.item_lista_planeta, parent, false);
+        TextView tvNome = itemlista.findViewById(R.id.txtNomePlaneta);
+        ImageView ivPlanet = itemlista.findViewById(R.id.imageView);
+        Planet planeta = (Planet)  mObjects[position];
+
+        tvNome.setText(planeta.nome);
+        ivPlanet.setImageResource(planeta.img);
+
+        return itemlista;
     }
 }
--- Lista/app/src/main/java/com/example/lista/MainActivity.java
+++ Lista/app/src/main/java/com/example/lista/MainActivity.java
@@ -11,9 +11,14 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    String [] planets = new String[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptuno"};
+    String [] planets = new String[] {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn"};
     Planet [] planetas = new Planet[]{
-            new Planet("Mercury", R.drawable.planeta)
+            new Planet("Mercury", R.drawable.mercurio),
+            new Planet("Venus", R.drawable.venus),
+            new Planet("Earth", R.drawable.terra),
+            new Planet("Mars", R.drawable.marte),
+            new Planet("Jupiter", R.drawable.jupiter),
+            new Planet("Saturn", R.drawable.saturno)
     };
     ListView lv;
 
@@ -23,24 +28,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ListView lv = findViewById(R.id.listView);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                this, android.R.layout.simple_list_item_1, android.R.id.text1,
-                planets);
+        AdapterPlanetas adapter = new AdapterPlanetas(this, R.layout.item_lista_planeta, planetas);
+        //ArrayAdapter<String>  adapter = new ArrayAdapter<>(
+        //        this,
+        //        android.R.layout.simple_list_item_1,
+        //        android.R.id.text1,
+        //        planetas
+        //);
         lv.setAdapter(adapter);
 
 
 
 
-        //adicionando tratamento de eventos click
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
 
-                Bundle b = new Bundle();
-                b.putString("planet", planets[i]);
-                intent.putExtras(b);
-
+                Bundle bundle = new Bundle();
+                bundle.putString("planet", planets[i]);
+                intent.putExtras(bundle);
                 startActivity(intent);
             }
         });
